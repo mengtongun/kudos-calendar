320b49d68582adf92d46a619d5adb2880ca6a2db
mengtongun <unmengtong@gmail.com>
2022-09-23 13:08:06 +0700

    feat: add favicon and title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,30 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import MainView from '@/views/MainView.vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import MainView from "@/views/MainView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: MainView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+	{
+		path: "/",
+		name: "home",
+		component: MainView,
+		meta: {
+			title: "Kudos Calendar",
+			description: "Kudos Calendar App",
+		},
+	},
+	{
+		path: "/about",
+		name: "about",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+	},
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+	history: createWebHashHistory(),
+	routes,
+});
 
-export default router
+export default router;
