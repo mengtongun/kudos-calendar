09dbe3a1f8bd3fb2b8824a4e56e98b6e202893b4
mengtongun <unmengtong@gmail.com>
2022-09-23 12:43:56 +0700

    chore: customize unauth page ui

--- src/App.vue
+++ src/App.vue
@@ -28,20 +28,4 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.amplify-button--primary {
-  background-color: #395b64;
-  color: #fff;
-  border: none;
-  border-radius: 0.5rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  font-weight: 500;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    background-color: #395b64;
-    color: #fff;
-    opacity: 0.8;
-  }
-}
 </style>
--- src/views/UnAuthView.vue
+++ src/views/UnAuthView.vue
@@ -1,17 +1,17 @@
 <template>
   <authenticator :form-fields="formFields">
     <template v-slot:header>
-      <div style="padding: var(--amplify-space-large); text-align: center; margin-bottom: -100px; margin-top: -100px">
-        <img class="amplify-image" alt="Amplify logo" src="logo.png" />
+      <div style="padding: var(--amplify-space-large); text-align: center; margin-bottom: -120px; margin-top: -120px">
+        <img class="amplify-image" alt="Amplify logo" src="logo.png" style="transform: scale(0.4)" />
       </div>
     </template>
 
     <template v-slot:sign-in-header>
-      <h3 class="amplify-heading" style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)">Sign in to your Kudos Calendar</h3>
+      <h3 class="amplify-heading" style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl); font-weight: bold">Sign in to your Kudos Calendar</h3>
     </template>
 
     <template v-slot:sign-up-header>
-      <h3 class="amplify-heading" style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)">Create a new Kudos Member</h3>
+      <h3 class="amplify-heading" style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl); font-weight: bold">Create a new Kudos Member</h3>
     </template>
 
     <template v-slot:footer>
@@ -28,6 +28,10 @@ const formFields = {
     username: {
       labelHidden: false,
       placeholder: "Enter your email",
+    },
+    password: {
+      labelHidden: false,
+      placeholder: "Enter your password",
     },
   },
   signUp: {},
@@ -74,5 +78,69 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.amplify-button--primary {
+  background-color: #5271ff;
+  color: #fff;
+  border: none;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #5271ff;
+    color: #fff;
+    opacity: 0.8;
+  }
+}
+.amplify-button--link {
+  background-color: transparent;
+  color: #5271ff;
+  border: none;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: #5271ff;
+    opacity: 0.8;
+  }
+}
+.amplify-input {
+  border: 1px solid #5271ff;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: 500;
+  width: 100%;
+  &:focus {
+    outline: none;
+    border: 1px solid #5271ff;
+  }
+  &::placeholder {
+    color: #5271ff;
+  }
+}
+[data-amplify-authenticator] [data-amplify-router] {
+  background-color: rgb(238, 248, 246);
+  border-bottom-left-radius: 1.5rem;
+  border-bottom-right-radius: 1.5rem;
+}
+.amplify-tabs-item[data-state="active"] {
+  background-color: #5271ff;
+  color: #fff;
+  border-color: grey;
+}
+.amplify-label {
+  font-size: 1rem;
+  font-weight: 500;
+  color: #5271ff;
+}
+.amplify-icon {
+  color: #5271ff;
+}
 </style>
