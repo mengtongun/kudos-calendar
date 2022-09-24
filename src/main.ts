858c02621b4da768ecb54ec6ca3bac6fab7cb660
mengtongun <unmengtong@gmail.com>
2022-09-24 18:08:25 +0700

    chore: add more prime component

--- src/main.ts
+++ src/main.ts
@@ -17,13 +17,28 @@ import Panel from "primevue/panel";
 import Menu from "primevue/menu";
 import Card from "primevue/card";
 import ColorPicker from "primevue/colorpicker";
-
+import Image from "primevue/image";
+import InputSwitch from "primevue/inputswitch";
+import InputNumber from "primevue/inputnumber";
+import Dropdown from "primevue/dropdown";
+import InputMask from "primevue/inputmask";
+import Checkbox from "primevue/checkbox";
+import RadioButton from "primevue/radiobutton";
+import MultiSelect from "primevue/multiselect";
+import Listbox from "primevue/listbox";
+import Calendar from "primevue/calendar";
+import Password from "primevue/password";
+import ToggleButton from "primevue/togglebutton";
+import Slider from "primevue/slider";
+import Rating from "primevue/rating";
+import Textarea from "primevue/textarea";
+import Editor from "primevue/editor";
 import FullCalendar from "@fullcalendar/vue3/dist/FullCalendar";
 
-import "primevue/resources/themes/saga-blue/theme.css"; //theme
-import "primevue/resources/themes/vela-blue/theme.css"; //theme
-import "primevue/resources/primevue.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
+import "primevue/resources/themes/saga-blue/theme.css"; // theme
+import "primevue/resources/themes/vela-blue/theme.css"; // theme
+import "primevue/resources/primevue.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
 import "primeflex/primeflex.scss";
 
 import AmplifyVue from "@aws-amplify/ui-vue";
@@ -48,6 +63,22 @@ app.component("Panel", Panel);
 app.component("Menu", Menu);
 app.component("Card", Card);
 app.component("ColorPicker", ColorPicker);
+app.component("Image", Image);
+app.component("InputSwitch", InputSwitch);
+app.component("InputNumber", InputNumber);
+app.component("Dropdown", Dropdown);
+app.component("InputMask", InputMask);
+app.component("Checkbox", Checkbox);
+app.component("RadioButton", RadioButton);
+app.component("MultiSelect", MultiSelect);
+app.component("Listbox", Listbox);
+app.component("Calendar", Calendar);
+app.component("Password", Password);
+app.component("ToggleButton", ToggleButton);
+app.component("Slider", Slider);
+app.component("Rating", Rating);
+app.component("Textarea", Textarea);
+app.component("Editor", Editor);
 
 app.component("FullCalendar", FullCalendar);
 
