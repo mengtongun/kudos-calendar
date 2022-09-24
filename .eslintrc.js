54869e6b085a82f3a60b31ef489f8ae4ba777c93
mengtongun <unmengtong@gmail.com>
2022-09-24 18:04:07 +0700

    chore: disable rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"vue/multi-word-component-names": "off",
+		"vue/script-setup-uses-vars": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
 	},
 };
