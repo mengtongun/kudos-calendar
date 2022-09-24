3c0303fb53be012097e1b9548817d6e73df703a0
mengtongun <unmengtong@gmail.com>
2022-09-24 18:07:54 +0700

    feat: add global constant default value

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// * DEFAULT VALUE FOR EVENT FORM
+export const FORM_DEFAULT_BG_COLOR = "#3f51b5";
+export const FORM_DEFAULT_BORDER_COLOR = "#3f51b5";
+export const FORM_DEFAULT_TEXT_COLOR = "#ffffff";
