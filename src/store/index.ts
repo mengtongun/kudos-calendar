1124d23aa0b68f1f4717252c756a9f122c101b16
mengtongun <unmengtong@gmail.com>
2022-09-24 18:10:32 +0700

    feat: vuex store event crud

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,114 @@
-import { createStore } from 'vuex'
+import {
+	FORM_DEFAULT_TEXT_COLOR,
+	FORM_DEFAULT_BORDER_COLOR,
+} from "./../constants/index";
+import { FORM_DEFAULT_BG_COLOR } from "@/constants";
+import { Events, StatusEnum } from "@/models";
+import { EventApiData } from "@/types";
+import { DataStore } from "aws-amplify";
+import { createStore } from "vuex";
+
+declare type UserInfo = {
+	id: string;
+	name: string;
+	email: string;
+};
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: { events: [] as Events[], userInfo: {} as UserInfo },
+	getters: {
+		getEvents: (state) => state.events,
+	},
+	mutations: {
+		ADD_EVENT(state, event) {
+			state.events.push(event);
+		},
+		DEL_EVENT(state, event) {
+			state.events = state.events.filter((e) => e.id !== event.id);
+		},
+		UPDATE_EVENT(state, event) {
+			state.events = state.events.map((e) => {
+				if (e.id === event.id) {
+					return event;
+				} else {
+					return e;
+				}
+			});
+		},
+	},
+	actions: {
+		async fetchEvents({ commit, state }) {
+			const events = (await DataStore.query(Events)).filter(
+				(e) => e.status === StatusEnum.ACTIVE
+			);
+			state.events = events;
+		},
+		addEvent({ commit }, event: EventApiData) {
+			commit("ADD_EVENT", event);
+			const data = {
+				status: StatusEnum.ACTIVE,
+				title: event.title,
+				description: event.description || "",
+				url: "",
+				start: event.start?.toISOString() || new Date().toISOString(), // will delete after finished other part
+				end: event.end?.toISOString() || new Date().toISOString(),
+				allDay: event.allDay,
+				classNames: "", // todo new feature on UI
+				editable: true,
+				startEditable: true,
+				durationEditable: true,
+				resourceEditable: true,
+				display: "",
+				overlap: true,
+				constraint: "",
+				backgroundColor: event.backgroundColor || FORM_DEFAULT_BG_COLOR,
+				borderColor: event.borderColor || FORM_DEFAULT_BORDER_COLOR,
+				textColor: event.textColor || FORM_DEFAULT_TEXT_COLOR,
+				extendedProps: "" /* Provide init commands */,
+				source: "",
+				groupId: event.groupId || "",
+				createdAt: new Date().toISOString(),
+				updatedAt: new Date().toISOString(),
+			};
+			console.log(data);
+			// return data;
+			return new Promise((resolve, reject) => {
+				DataStore.save(new Events(data))
+					.then((res) => {
+						resolve(res);
+					})
+					.catch((err) => {
+						console.log("ADD_EVENT network error", err);
+						reject(err);
+					});
+			});
+		},
+		delEvent({ commit }, event) {
+			commit("DEL_EVENT", event);
+			return new Promise((resolve, reject) => {
+				DataStore.delete(Events, event.id)
+					.then((res) => {
+						resolve(res);
+					})
+					.catch((err) => {
+						console.log("DEL_EVENT network error", err);
+						reject(err);
+					});
+			});
+		},
+		updateEvent({ commit }, event) {
+			commit("UPDATE_EVENT", event);
+			return new Promise((resolve, reject) => {
+				DataStore.save(Events.copyOf(event, (updated) => updated))
+					.then((res) => {
+						resolve(res);
+					})
+					.catch((err) => {
+						console.log("UPDATE_EVENT network error", err);
+						reject(err);
+					});
+			});
+		},
+	},
+	modules: {},
+});
